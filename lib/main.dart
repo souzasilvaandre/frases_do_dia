70a8344336115efdc90236beb91026d8aefee3f7
André Luis <andre_souzasilva@hotmail.com>
2023-04-09 16:17:16 -0300

    formatação de texto

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(const MaterialApp(
-    title: 'Frases do dia',
-    home: Row(children: [
-      Text("t1 "),
-      Text("t2 "),
-      Text("t3")
-    ],)
+  runApp(MaterialApp(
+    title: "frase do dia",
+    home: Container(
+      color: Colors.white,
+      child: const Column(children: [
+        Text(
+          "André Luis",
+          style: TextStyle(
+            fontSize: 50,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 0,
+            wordSpacing: 0,
+            color: Colors.black,
+            decoration: TextDecoration.underline,
+            decorationColor: Color.fromARGB(255, 209, 63, 10),
+            decorationStyle: TextDecorationStyle.solid,
+          ),
+          )
+      ],),
+    ),
   ));
 }
