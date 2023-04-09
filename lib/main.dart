3f79069f5069d94b9b85b040460bd076f988bcd8
André Luis <andre_souzasilva@hotmail.com>
2023-04-09 16:43:26 -0300

    textbutton

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,18 @@ void main(List<String> args) {
     title: "frase do dia",
     home: Container(
       color: Colors.white,
-      child: const Column(children: [
-        Text(
-          "André Luis",
-          style: TextStyle(
-            fontSize: 50,
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0,
-            wordSpacing: 0,
-            color: Colors.black,
-            decoration: TextDecoration.underline,
-            decorationColor: Color.fromARGB(255, 209, 63, 10),
-            decorationStyle: TextDecorationStyle.solid,
-          ),
-          )
+      child:  Column(children: [
+        TextButton(
+          onPressed: (){ 
+            print("Botão apertado");
+            }, 
+          child: const Text(
+            "Clique aqui",
+            style: TextStyle(
+              fontSize: 50,
+              color: Colors.blue
+            ) ,
+            ))
       ],),
     ),
   ));
