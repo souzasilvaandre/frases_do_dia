635107ca97a2952fbed7991d3e34b04b8b924aa4
André Luis <andre_souzasilva@hotmail.com>
2023-04-09 21:47:30 -0300

    espaçamento

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,19 @@ void main(List<String> args) {
   runApp(MaterialApp(
     title: "frase do dia",
     home: Container(
-      color: Colors.white,
-      child:  Column(children: [
-        TextButton(
-          onPressed: (){ 
-            print("Botão apertado");
-            }, 
-          child: const Text(
-            "Clique aqui",
-            style: TextStyle(
-              fontSize: 50,
-              color: Colors.blue
-            ) ,
-            ))
+      color: Colors.black,
+      margin: EdgeInsets.all(20),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: const Column(children: [
+        Text(" It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',",
+        textAlign: TextAlign.justify,
+        style: TextStyle(
+          fontSize: 30,
+          color: Colors.white,
+          decoration: TextDecoration.none
+        ),)
       ],),
+     
     ),
   ));
 }
