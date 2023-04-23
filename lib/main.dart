3f529c906eafc12684550b30a0b12e5bb6feeba1
André Luis <andre_souzasilva@hotmail.com>
2023-04-23 18:23:17 -0300

    alinhamento

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,21 @@ void main(List<String> args) {
   runApp(MaterialApp(
     title: "frase do dia",
     home: Container(
-      //color: Colors.black,
-      margin: const EdgeInsets.all(20),
-      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+      margin: EdgeInsets.only(top: 40),
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 12, 80, 174),
-        border: Border.all(
-          width: 8, color: Colors.white
-        ),
-        borderRadius: BorderRadius.circular(12),
+        border: Border.all(width: 3,
+        color: Colors.white
+        )
       ),
-      child: const Column(children: [
-        Text(" It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',",
-        textAlign: TextAlign.justify,
-        style: TextStyle(
-          fontSize: 30,
-          color: Colors.white,
-          decoration: TextDecoration.none
-        ),)
-      ],),
-     
+     child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text("T1"),
+        Text("T2"),
+        Text("T3")
+      ],
+     ),
     ),
   ));
 }
