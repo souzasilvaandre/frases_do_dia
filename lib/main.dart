5aab2cc406147b3817cec7350cbcc5c11659269b
André Luis <andre_souzasilva@hotmail.com>
2023-05-07 22:40:48 -0300

    Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,55 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(MaterialApp(
     title: "frase do dia",
-    home: Scaffold(
+    home: HomeStateSull(),
+  ));
+}
+
+class HomeStateSull extends StatefulWidget {
+  const HomeStateSull({super.key});
+   
+  @override
+  State<HomeStateSull> createState() => _HomeStateSullState();
+}
+
+class _HomeStateSullState extends State<HomeStateSull> {
+  var _texto = "André Luis";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title:  Text("Instagran"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            ElevatedButton(
+                child: Text('clique aqui'), 
+                onPressed: () {
+                  setState(() {
+                    _texto = "Andrea Avezun";
+                  });
+                },
+              ),
+              Text(_texto)
+          ],
+        ),
+      ),
+        
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  final _titulo = "Instagran";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text(_titulo),
         backgroundColor: Colors.green,
       ),
       body: Padding(
@@ -24,6 +70,6 @@ void main(List<String> args) {
             ),
             ),
         ),
-    ),
-  ));
+    );
+  }
 }
