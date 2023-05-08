057414afa00caba0d40442e0ce7ff491b37d0480
André Luis <andre_souzasilva@hotmail.com>
2023-05-07 22:12:00 -0300

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,27 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(MaterialApp(
     title: "frase do dia",
-    home: Container(
-      margin: EdgeInsets.only(top: 40),
-      decoration: BoxDecoration(
-        border: Border.all(width: 3,
-        color: Colors.white
-        )
+    home: Scaffold(
+      appBar: AppBar(
+        title:  Text("Instagran"),
+        backgroundColor: Colors.green,
       ),
-     child: Image.asset(
-      "images/robo.jpg",
-      fit: BoxFit.contain,
-     ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Text("Principal"),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.lightGreen,
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Text("texto1"),
+                Text("texto1"),
+              ],
+            ),
+            ),
+        ),
     ),
   ));
 }
