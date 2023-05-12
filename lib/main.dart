6b93ad1adc1f8e616fddb6b047cd7fda8ae01bf2
André Luis <andre_souzasilva@hotmail.com>
2023-05-12 14:17:35 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
-    title: "frase do dia",
+    title: "frase do dia 2",
     home: HomeStateSull(),
   ));
 }
