bbf9731efdd0f56949c53d756eb8168b8b3afac9
André Luis <andre_souzasilva@hotmail.com>
2023-05-21 17:27:42 -0300

    21-05

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-    title: "frase do dia 2",
-    home: HomeStateSull(),
-  ));
+  runApp(
+    MaterialApp(
+      home: Container() ,
+    ),
+  );
 }
 
-class HomeStateSull extends StatefulWidget {
-  const HomeStateSull({super.key});
-   
-  @override
-  State<HomeStateSull> createState() => _HomeStateSullState();
-}
-
-class _HomeStateSullState extends State<HomeStateSull> {
-  var _texto = "André Luis";
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:  Text("Instagran"),
-        backgroundColor: Colors.green,
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            ElevatedButton(
-                child: Text('clique aqui'), 
-                onPressed: () {
-                  setState(() {
-                    _texto = "Andrea Avezun";
-                  });
-                },
-              ),
-              Text(_texto)
-          ],
-        ),
-      ),
-        
-    );
-  }
-}
-
-
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
-  final _titulo = "Instagran";
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:  Text(_titulo),
-        backgroundColor: Colors.green,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Text("Principal"),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.lightGreen,
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Row(
-              children: [
-                Text("texto1"),
-                Text("texto1"),
-              ],
-            ),
-            ),
-        ),
-    );
+    return const Placeholder();
   }
 }
